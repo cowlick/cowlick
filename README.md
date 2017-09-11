682582ac65951f0f9dcee57218e44d739505c9d2
pocketberserker <pocketberserker@gmail.com>
2017-09-11 13:11:45 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cowlick
+
+`cowlick` is a novel engine for [Akashic Engine](https://github.com/akashic-games/akashic-engine).
+
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var srcDir = path.join(__dirname, "src");
+var outDir = path.join(__dirname, "lib");
+var isProd = process.env.NODE_ENV === 'production';
+
+var config = {
+  entry: path.join(srcDir, "index.ts"),
+  output : {
+    path: outDir,
+    filename: "index.js",
+    library: "cowlick",
+    libraryTarget: "commonjs"
+  },
+  module : {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader",
+        include: srcDir,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts"]
+  },
+  plugins: [
+  ],
+  node: {
+    fs: "empty"
+  }
+};
+
+if (isProd) {
+  config.plugins = [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    })
+  ]
+    .concat(config.plugins)
+    .concat([
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        output  : {
+          comments: require("uglify-save-license")
+        }
+      })
+    ]);
+}
+module.exports = config;
