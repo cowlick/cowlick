1a7cc1c429b40ac14e0bb99c2d59e1ce5b5f5fd4
pocketberserker <pocketberserker@gmail.com>
2018-02-13 19:31:36 +0900

    エントリポイントでg.GameMainParameterObjectを受け取れる形に修正

--- packages/sample/src/main.ts
+++ packages/sample/src/main.ts
@@ -1,13 +1,17 @@
 "use strict";
 import {engine} from "cowlick-engine";
 
-module.exports = () => {
-  g._require(g.game, "load")(engine);
-  const controller = engine.load("first");
+module.exports = (param: g.GameMainParameterObject) => {
+  if (param.snapshot) {
+    require("snapshotLoader")(param.snapshot);
+  } else {
+    require("load")(engine);
+    const controller = engine.load("first");
 
-  g.game.snapshotRequest.add(() => {
-    if (g.game.shouldSaveSnapshot()) {
-      g.game.saveSnapshot(controller.snapshot());
-    }
-  });
+    g.game.snapshotRequest.add(() => {
+      if (g.game.shouldSaveSnapshot()) {
+        g.game.saveSnapshot(controller.snapshot());
+      }
+    });
+  }
 };
