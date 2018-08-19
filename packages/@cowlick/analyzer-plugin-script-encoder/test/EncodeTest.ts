06e03fc95b5c8dcff68f4e9708c8ad7ed3595765
pocketberserker <pocketberserker@gmail.com>
2018-08-20 05:11:22 +0900

    fix test name

--- packages/@cowlick/analyzer-plugin-script-encoder/test/EncodeTest.ts
+++ packages/@cowlick/analyzer-plugin-script-encoder/test/EncodeTest.ts
@@ -3,8 +3,8 @@ import * as fs from "fs";
 import * as esprima from "esprima";
 import {encodeFrame} from "../src/encode";
 
-describe("encode", () => {
-  it("FrameをEncodeに変換できる", () => {
+describe("script-encoder-plugin", () => {
+  it("FrameをEncodedFrameに変換できる", () => {
     const input = esprima.parseModule(fs.readFileSync("test/input.js", "utf8"));
     const expected = esprima.parseModule(fs.readFileSync("test/expected.js", "utf8"));
     assert.deepEqual(encodeFrame(input), expected);
