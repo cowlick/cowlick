377d970ccf9a1bf1a8704c63b9ec923cef1cc701
pocketberserker <pocketberserker@gmail.com>
2019-06-03 08:05:16 +0900

    ビルド設定ファイルを使って生成できるようにコマンドを変更

--- packages/@cowlick/kag-compiler/src/cli.ts
+++ packages/@cowlick/kag-compiler/src/cli.ts
@@ -11,30 +11,67 @@ const packageJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../packa
 interface CompileOpts {
   outdir: string[];
   plugin: string[];
+  config: string[];
 }
 
 interface CompileArgs {
   sources: string[];
 }
 
+interface Config {
+  outdir: string;
+  plugins: string[];
+  sources: string[];
+}
+
 const root = commandpost
-  .create<CompileOpts, CompileArgs>("cowlick-kag-compiler <sources...>")
+  .create<CompileOpts, CompileArgs>("cowlick-kag-compiler [sources...]")
   .version(packageJson.version, "-v, --version")
   .description("compile KAG scenario")
   .option("-o, --outdir <outdir>", "output dir")
   .option("-p, --plugin <path>", "plugin path")
+  .option("-c, --config <path>", "config JSON file path for cowlick KAG compiler")
   .action(async (opts, args) => {
-    const outdir: string = opts.outdir[0] || "script";
+    const config: Config = {
+      outdir: "script",
+      plugins: [],
+      sources: []
+    };
     const cwd = process.cwd();
-    const outputPath = path.resolve(cwd, outdir);
+    if (opts.config && opts.config[0]) {
+      const configJson = JSON.parse(fs.readFileSync(path.resolve(cwd, opts.config[0]), "utf8"));
+      if ("outdir" in configJson) {
+        config.outdir = configJson.outdir;
+      }
+      if ("plugins" in configJson) {
+        config.plugins.push(...configJson.plugins);
+      }
+      if ("sources" in configJson) {
+        config.sources.push(...configJson.sources);
+      }
+    } else {
+      if (opts.outdir && opts.outdir[0]) {
+        config.outdir = opts.outdir[0];
+      }
+      if (opts.plugin) {
+        config.plugins.push(...opts.plugin);
+      }
+      if (args.sources && args.sources.length > 0) {
+        config.sources.push(...args.sources);
+      } else {
+        console.log("`sources` more than one argument");
+        process.exit(1);
+      }
+    }
+    const outputPath = path.resolve(cwd, config.outdir);
     try {
       await analyzer.mkdir(outputPath);
     } catch (e) {
       console.log("output directory already exists: " + outputPath);
     }
-    for (const source of args.sources) {
+    for (const source of config.sources) {
       const target = path.resolve(cwd, source);
-      const plugins = (opts.plugin ? opts.plugin : []).map(p => new analyzer.Plugin(path.resolve(cwd, p)));
+      const plugins = config.plugins.map(p => new analyzer.Plugin(path.resolve(cwd, p)));
       const ast = await parse(target, runProgress);
       const result = await runProgress("Analyzing scenario", async () => await analyzer.analyze(ast, plugins));
       await runProgress(`Generate scenario`, async () => analyzer.generate(outputPath, result.scenario));
